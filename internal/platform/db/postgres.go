4579214e45faf617945e94b50863943bcd500059
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-03-18 20:26:21 +0100

    NewPool function

--- internal/platform/db/postgres.go
+++ internal/platform/db/postgres.go
@@ -1,3 +1,22 @@
 package db
 
-type Client struct{}
+import (
+	"context"
+
+	"github.com/Mozlook/fotobudka-backend/internal/config"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool(ctx context.Context, cfg config.Config) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(ctx, cfg.DB.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		pool.Close()
+		return nil, err
+	}
+	return pool, nil
+}
