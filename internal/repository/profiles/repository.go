3030cf5244110024d3afb72df7984091818bb965
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-03-19 23:32:43 +0100

    UpsertPhotographerProfile

--- internal/repository/profiles/repository.go
+++ internal/repository/profiles/repository.go
@@ -41,3 +41,35 @@ func (r *Repository) GetPhotographerProfileByUserID(ctx context.Context, userID 
 		UpdatedAt:   row.UpdatedAt,
 	}, nil
 }
+
+func (r *Repository) UpsertPhotographerProfile(ctx context.Context, in UpsertInput) (Profile, error) {
+	socialLinksJson, err := json.Marshal(in.SocialLinks)
+	if err != nil {
+		return Profile{}, fmt.Errorf("socialLinks parsing error: %w", err)
+	}
+	row, err := r.q.UpsertPhotographerProfile(ctx, dbgen.UpsertPhotographerProfileParams{
+		UserID:      in.UserID,
+		Username:    in.Username,
+		DisplayName: in.DisplayName,
+		Bio:         in.Bio,
+		SocialLinks: socialLinksJson,
+	})
+	if err != nil {
+		return Profile{}, fmt.Errorf("upsert photographer profile: %w", err)
+	}
+
+	var socialLinksReturn SocialLinks
+	err = json.Unmarshal(row.SocialLinks, &socialLinksReturn)
+	if err != nil {
+		return Profile{}, fmt.Errorf("socialLinks parsing error: %w", err)
+	}
+	return Profile{
+		UserID:      row.UserID,
+		Username:    row.Username,
+		DisplayName: row.DisplayName,
+		Bio:         row.Bio,
+		SocialLinks: socialLinksReturn,
+		CreatedAt:   row.CreatedAt,
+		UpdatedAt:   row.UpdatedAt,
+	}, nil
+}
