84731376d4ff162ad9d105409838aad4c3284048
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-03-19 09:32:55 +0100

    extended authHandler with repository

--- internal/http/handler/auth/handler.go
+++ internal/http/handler/auth/handler.go
@@ -3,19 +3,22 @@ package auth
 import (
 	"github.com/Mozlook/fotobudka-backend/internal/config"
 	"github.com/Mozlook/fotobudka-backend/internal/oauth"
+	"github.com/Mozlook/fotobudka-backend/internal/repository/users"
 )
 
 // AuthHandler serves HTTP endpoints related to authentication.
 type AuthHandler struct {
 	cfg      config.Config
 	provider *oauth.Provider
+	userRepo *users.Repository
 }
 
 // NewAuthHandler creates an AuthHandler configured with application settings
 // and the Google OAuth provider.
-func NewAuthHandler(cfg config.Config, provider *oauth.Provider) *AuthHandler {
+func NewAuthHandler(cfg config.Config, provider *oauth.Provider, repo *users.Repository) *AuthHandler {
 	return &AuthHandler{
 		cfg:      cfg,
 		provider: provider,
+		userRepo: repo,
 	}
 }
