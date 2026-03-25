e266119f6fe730c51a701b1370604c22e2364163
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-03-25 09:30:09 +0100

    InsertSessionAccess query

--- internal/platform/db/query/session_access.sql
+++ internal/platform/db/query/session_access.sql
@@ -1,9 +1,11 @@
 -- name: InsertSessionAccess :one
 INSERT INTO session_access (
+  id,
   session_id,
   code_hmac,
   token_hmac
 ) VALUES (
+sqlc.arg(id),
 sqlc.arg(session_id),
 sqlc.arg(code_hmac),
 sqlc.arg(token_hmac)
