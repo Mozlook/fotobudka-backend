59e8074087c617e5af3e207c7596a24e259792b3
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-03-27 14:16:40 +0100

    retrieve by token method

---
+++ internal/sessionaccess/retrieve.go
@@ -0,0 +1,25 @@
+package sessionaccess
+
+import (
+	"context"
+	"errors"
+
+	"github.com/Mozlook/fotobudka-backend/internal/repository/sessions"
+	"github.com/jackc/pgx/v5"
+)
+
+var ErrAccessNoFound = errors.New("access not found")
+
+func (s *Service) GetClientSessionByTokenHMAC(ctx context.Context, token string) (sessions.ClientSession, error) {
+	tokenHMAC := hmacHex(s.secret, token)
+
+	clientSession, err := s.repo.GetClientSessionByTokenHMAC(ctx, tokenHMAC)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return sessions.ClientSession{}, ErrAccessNoFound
+		}
+
+		return sessions.ClientSession{}, err
+	}
+	return clientSession, nil
+}
