1b43cb974e92e0547532d4659f8703e1a4f55f74
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-03-31 18:29:37 +0200

    StatObject

---
+++ internal/platform/storage/object.go
@@ -0,0 +1,28 @@
+package storage
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/minio/minio-go/v7"
+)
+
+var ErrObjectNotFound = errors.New("object with that ey doesn't exist")
+
+func (c *Client) StatObject(ctx context.Context, objectKey string) (minio.ObjectInfo, error) {
+	if objectKey == "" {
+		return minio.ObjectInfo{}, fmt.Errorf("object_key cannot be empty")
+	}
+
+	object, err := c.minio.StatObject(ctx, c.bucketName, objectKey, minio.StatObjectOptions{})
+	if err != nil {
+		minioErr := minio.ToErrorResponse(err)
+		if minioErr.Code == minio.NoSuchKey {
+			return minio.ObjectInfo{}, ErrObjectNotFound
+		}
+		return minio.ObjectInfo{}, err
+	}
+
+	return object, nil
+}
