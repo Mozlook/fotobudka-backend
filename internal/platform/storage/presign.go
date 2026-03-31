d1ee5d3f1f1a0543d42d773ee1f518ed3e3042d9
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-03-31 16:15:56 +0200

    PresignedPutObject

---
+++ internal/platform/storage/presign.go
@@ -0,0 +1,25 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"time"
+)
+
+func (c *Client) PresignedPutObject(ctx context.Context, objectKey string, expires time.Duration) (*url.URL, error) {
+	if objectKey == "" {
+		return nil, fmt.Errorf("object_key cannot be empty")
+	}
+
+	if expires < time.Second {
+		return nil, fmt.Errorf("duration must be longer than 1 second")
+	}
+
+	putURL, err := c.minio.PresignedPutObject(ctx, c.bucketName, objectKey, expires)
+	if err != nil {
+		return nil, err
+	}
+
+	return putURL, nil
+}
