2aeacd53b698ab34bb9e1499cf18baba563c16ca
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-04-03 05:30:19 +0200

    UploadPhoto queries

---
+++ internal/platform/db/query/session_photos.sql
@@ -0,0 +1,19 @@
+-- name: GetSessionPhotoByIIDAndSessionID :one
+SELECT
+    id,
+    session_id,
+    source_key,
+    status,
+    source_size_bytes
+FROM session_photos
+WHERE id = $1
+AND session_id = $2;
+
+-- name: MarkSessionPhotoUploaded :execrows
+UPDATE session_photos
+SET
+  status = 'uploaded',
+  source_size_bytes = $1
+WHERE id = $2
+AND session_id = $3;
+
