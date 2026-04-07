08ce7c450ce5d3eb94e466219d622784bb817924
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-04-08 00:45:33 +0200

    minio storage get and put object methods

--- internal/platform/storage/object.go
+++ internal/platform/storage/object.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
 
 	"github.com/minio/minio-go/v7"
 )
@@ -25,4 +26,61 @@ func (c *Client) StatObject(ctx context.Context, objectKey string) (minio.Object
 	}
 
 	return object, nil
+}
+
+func (c *Client) GetObject(ctx context.Context, objectKey string) (*minio.Object, error) {
+	if objectKey == "" {
+		return nil, fmt.Errorf("object_key cannot be empty")
+	}
+
+	object, err := c.internalMinio.GetObject(ctx, c.bucketName, objectKey, minio.GetObjectOptions{})
+	if err != nil {
+		minioErr := minio.ToErrorResponse(err)
+		if minioErr.Code == minio.NoSuchKey {
+			return nil, ErrObjectNotFound
+		}
+		return nil, err
+	}
+
+	if _, err := object.Stat(); err != nil {
+		_ = object.Close()
+
+		minioErr := minio.ToErrorResponse(err)
+		if minioErr.Code == minio.NoSuchKey {
+			return nil, ErrObjectNotFound
+		}
+		return nil, err
+	}
+
+	return object, nil
+}
+
+func (c *Client) PutObject(ctx context.Context, objectKey, contentType string, reader io.Reader, size int64) error {
+	if objectKey == "" {
+		return fmt.Errorf("object_key cannot be empty")
+	}
+
+	if contentType == "" {
+		return fmt.Errorf("content_type cannot be empty")
+	}
+
+	if size <= 0 {
+		return fmt.Errorf("size must be greater than 0")
+	}
+
+	_, err := c.internalMinio.PutObject(
+		ctx,
+		c.bucketName,
+		objectKey,
+		reader,
+		size,
+		minio.PutObjectOptions{
+			ContentType: contentType,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
