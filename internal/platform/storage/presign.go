ef939a481f94ea5ccaf2eae65e23ee40baf45e80
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-04-14 01:38:16 +0200

    PresignedGetObject

--- internal/platform/storage/presign.go
+++ internal/platform/storage/presign.go
@@ -23,3 +23,20 @@ func (c *Client) PresignedPutObject(ctx context.Context, objectKey string, expir
 
 	return putURL, nil
 }
+
+func (c *Client) PresignedGetObject(ctx context.Context, objectKey string, expires time.Duration) (string, error) {
+	if objectKey == "" {
+		return "", fmt.Errorf("object_key cannot be empty")
+	}
+
+	if expires < time.Second {
+		return "", fmt.Errorf("duration must be longer than 1 second")
+	}
+
+	getURL, err := c.publicMinio.PresignedGetObject(ctx, c.bucketName, objectKey, expires, url.Values{})
+	if err != nil {
+		return "", fmt.Errorf("presign get object: %w", err)
+	}
+
+	return getURL.String(), nil
+}
